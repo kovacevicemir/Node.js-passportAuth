f4c842a8d7a2f0f1a63919637fe46f6e2d19a066
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-26 11:50:46 +1000

    EJS implementation and views design

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', (req,res) => res.send('Welcome to this shit'));
+router.get('/', (req,res) => res.render('../views/welcome'));
 
 module.exports = router;
