8e6d9d2a9b3bb476398a4defd8916c37455ab36e
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-26 16:05:04 +1000

    register post handler and form validation, including body parser and req.body

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+const { Mongoose } = require('mongoose');
+const mongoose = require('mongoose')
 
 const app = express();
 
+//DB Config
+const db = require('./config/keys').MongoURI
+
+//connect to mongo
+mongoose.connect(db, {useNewUrlParser:true})
+    .then(() => console.log('mgdb connected'))
+    .catch(err => console.log(err))
+
+//Midlewares:
 //EJS
-//midleware
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
+
+//BodyParser -> need this in order to request data from form body... req.body
+app.use(express.urlencoded({extended:false}))
 
 //Routes
 app.use('/', require('./routes/index'))
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,39 @@ router.get('/login', (req,res) => res.render('Login'));
 //Register
 router.get('/register', (req,res) => res.render('register'));
 
+//Register Handle
+router.post('/register', (req,res) => {
+    const {name,email,password,password2} = req.body
+    let errors = []
+
+    //check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({ msg:'please fill in all fields' })
+    }
+
+    //check passwords match
+    if(password !== password2){
+        errors.push({ msg: 'Passwords do not match'})
+    }
+
+    //Check if password > 6
+    if(password.length < 6){
+        errors.push({ msg:'Password should be at least 6 characters' })
+    }
+
+    if(errors.length > 0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    }else{
+        res.send('pass')
+    }
+})
+
 //Dashboard
 router.get('/dashboard', (req,res) => res.render('dashboard'))
 
